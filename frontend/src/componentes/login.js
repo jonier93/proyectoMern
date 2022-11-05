f15b6e630ce2ea4bf87a902b08b4b1f68d008303
jonier93 <jonietrporras@gmail.com>
2022-11-04 19:04:59 -0500

    version2

--- frontend/src/componentes/login.js
+++ frontend/src/componentes/login.js
@@ -4,19 +4,29 @@ import '../styles/styleLogin.css'
 
 export function Login (){
     return(
-        <div>
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-4" id="columna1"><h1> Columna 1</h1></div>
-                    <div class="col-4" id="columna2"> <h1> Columna 2</h1></div>
-                    <div class="col-4" id="columna3"> <h1> Columna 3</h1></div>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col"></div>
+                <div class="col" id="colPers">
+                    <img id="imagenLogin" src="https://www.aliwell.mx/imagenes/registro/icono_sesion.png"></img>
+                    <div class="row">
+                        <div class="col"> <label> Email </label> </div>
+                        <div class="col"> <input type="email" id="emailCred" placeholder="ingrese su email" /> </div>
+                    </div>
+                    <div class="row"> &nbsp; </div>
+                    <div class="row">
+                        <div class="col"> <label> Contraseña </label> </div>
+                        <div class="col"> <input type="password" id="passwordCred" placeholder="ingrese su contraseña" /> </div>  
+                    </div>
+                    <div class="row"> &nbsp; </div>
+                    <div class="row">
+                        <div class="col-2"></div>
+                        <div class="col-8"> <button type="button" class="btn btn-primary btn-lg" > Logear </button> </div>
+                        <div class="col-2"></div>
+                    </div>
                 </div>
-                <div class="row">
-                    <div class="col" id="columna4"> <h1> Columna 4 </h1></div>
-                    <div class="col" id="columna5"> <h1> Columna 5 </h1></div>
-                </div>
+                <div class="col"></div>
             </div>
         </div>
-
     )    
 }
